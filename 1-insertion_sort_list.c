e8158de7dd34ebd60eded4c3c3cb3ce63292fede
gideon <abaaneg@gmail.com>
2023-04-25 23:34:19 +0000

    insertion list debbugginh

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,47 +1,35 @@
-#include <stdio.h>  // Include the necessary header file
+#include <stdio.h>
 
 #include "sort.h"
+/**
+* insertion_sort_list - insertion sorting algorithm
+* @list: linked list to sort
+*/
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *tmp;
+	int n;
 
-/** 
- * insertion_sort_list - insertion sorting algorithm 
- * @list: linked list to sort 
- */
-void insertion_sort_list(listint_t **list) {
-    listint_t *tmp;
-    int n;
-    
-    if (!list)
-        return;
-    
-    tmp = *list;
-    while (tmp) {
-        while (tmp) {
-            if (tmp->next) {
-                if (tmp->n > tmp->next->n) {
-                    n = tmp->n;
-                    *(int *)&tmp->n = tmp->next->n;
-                    *(int *)&tmp->next->n = n;
-                    tmp = *list;
-                    print_list(*list);
-                    break;
-                }
-            }
-            tmp = tmp->next;
-        }
-    }
+	if (!list)
+		return;
+	tmp = *list;
+	while (tmp)
+	{
+		while (tmp)
+		{
+			if (tmp->next)
+			{
+				if (tmp->n > tmp->next->n)
+				{
+					n = tmp->n;
+					*(int *)&tmp->n = tmp->next->n;
+					*(int *)&tmp->next->n = n;
+					tmp = *list;
+					print_list(*list);
+					break;
+				}
+			}
+			tmp = tmp->next;
+		}
+	}
 }
-
-/** 
- * print_list - prints all the elements of a listint_t list 
- * @h: pointer to head of list 
- */
-void print_list(const listint_t *h) {
-    while (h) {
-        printf("%d", h->n);
-        if (h->next)
-            printf(", ");
-        h = h->next;
-    }
-    printf("\n");
-}
-
